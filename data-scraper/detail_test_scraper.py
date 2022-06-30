117b8cf05070a43344bad86362fcab4f6bdfae5d
skynetnfb <46841502+skynetnfb@users.noreply.github.com>
2022-06-30 17:25:26 +0200

    scraping in json

--- data-scraper/detail_test_scraper.py
+++ data-scraper/detail_test_scraper.py
@@ -3,7 +3,6 @@ from cgitb import html
 import requests
 import bs4
 
-
 def detail_scraper(address,name,element):
     url = address+name
     res =  requests.get(url)
@@ -58,9 +57,10 @@ def detail_scraper(address,name,element):
                         if li.get_text(strip=True)!='':
                             #print(li.get_text(strip=True)[0].isdigit())
                             compatible_boards_list = compatible_boards_list + '"'+str(li.get_text(strip=True))+'",'
+                            compatible_boards_list=compatible_boards_list + ','
                     #print(compatible_boards_list)
-
-                    compatible_boards_list= compatible_boards_list[:-1]
+                    if(compatible_boards_list[:-1]==','):
+                        compatible_boards_list= compatible_boards_list[:-1]
                     data.write('"boards":'+compatible_boards_list+']\n') 
                 #else:
                 #    data.write('"boards":[],'+'\n')
@@ -82,53 +82,4 @@ def detail_scraper(address,name,element):
         print ('------------------------------------'+url)
 
 
-
-def category_scraper(url = 'https://www.arduino.cc/reference/en/libraries/category/communication/'):
-    res =  requests.get(url)
-    try:
-        res.raise_for_status()
-    except Exception as exc:
-        print('Exception raised')
-
-    html_page = bs4.BeautifulSoup(res.text, 'html.parser')
-
-    elem_html = 'div.single-page'
-    sel_elem = html_page.select(elem_html)
-
-    text_elem = sel_elem[0].getText()
-    container = html_page.find_all("li")
-    print(len(container))
-    with open('library_test.txt','a') as data1:
-        data1.write('['+'\n')
-    with open('data.txt','w') as data:
-
-        for li in html_page.find_all("li"):
-            #print(li.get_text(strip=True).encode("utf-8"))
-            parsed_name = str(li.get_text(strip=True).encode("utf-8")).split(":", 1)[0]
-            
-            if(parsed_name[1] == '"'):
-                parsed_name = parsed_name.split('b"', 1)[1]
-                #print(parsed_name)
-                #data.write(parsed_name+','+'\n')
-            else:
-                parsed_name = parsed_name.split("b'", 1)[1]
-                #print(parsed_name)
-                #data.write(parsed_name+','+'\n')
-            parsed_name = parsed_name.split("'", 1)[0]
-            
-            if(str(parsed_name) != 'Languagefunctionsvariablesstructure' 
-            and str(parsed_name) != 'functions'  
-            and str(parsed_name) != 'variables' 
-            and str(parsed_name) != 'structure'
-            and str(parsed_name) != 'Libraries'
-            and str(parsed_name) != 'IoT Cloud API'
-            and str(parsed_name) != 'Glossary'):
-                data.write(parsed_name+','+'\n')                
-                with open('library_test.txt','a') as data1:
-                    detail_scraper(address='https://www.arduino.cc/reference/en/libraries/',name=parsed_name.replace(' ','-').lower(),element='div.single-page')
-                    data1.write(','+'\n')
-    with open('library_test.txt','a') as data1:
-                data1.write(']'+'\n')
-    print('file data updated')
-
-category_scraper ( url='https://www.arduino.cc/reference/en/libraries/category/communication/')
+detail_scraper('https://www.arduino.cc/reference/en/libraries/','arduinoble','div.single-page')
