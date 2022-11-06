2c200d369e620cbf3063bc7dfd61170ee5643404
skynetnfb <46841502+skynetnfb@users.noreply.github.com>
2022-11-06 19:38:11 +0100

    viewgen

--- resyduo_web_app/src/static/js/codegen.js
+++ resyduo_web_app/src/static/js/codegen.js
@@ -1,127 +1,4 @@
-function statementContainerView(parentID, cardName, stype, bodyId) {
-    //var parent = document.getElementById(parentID)
-    var card = document.createElement('div')
-    card.className = "card bg-light mb-3"
-    card.id = stype
-    card.innerHTML = `
-    <div class="card-header justify-content-between">
-    <div class = "row">    
-        <div class = "col">
-                <h5>`+ cardName + `</h5>    
-            </div>
-            <div class = "col d-flex justify-content-end align-items-center">
-                <span class="badge text-bg-secondary" onClick = "this.parentNode.parentNode.parentNode.parentNode.remove()">&#x2715</span>
-            </div>
-        </div>
-    </div>
-    <div class="card-body" id="`+ bodyId + `" stype="` + stype + `" parentCode ="`+parentID+`" >
-    </div>
-    `
-    return card
-}
-
-
-function variableViewGenerator(parentID, statement) {
-    bodyId = 'body' + Date.now()
-    card = statementContainerView(parentID, statement, statement, bodyId)
-    var body = card.querySelector('#' + bodyId)
-    body.innerHTML = `
-<div class="row">
-    <div class="col">
-        <div class="input-group mb-3">
-            <select class="form-select" id="variableType">
-                <option selected value="int">int</option>
-                <option value="char">char</option>
-                <option value="long">long</option>
-                <option value="float">float</option>
-                <option value="variable">double</option>
-            </select>
-            <label class="input-group-text" for="inputGroupSelectLibrariesTab1">Type</label>
-        </div>
-    </div>
-    <div class="col">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="x" aria-label="Variable"
-                aria-describedby="basic-addon2" id="variableId" value ="x">
-            <div class="input-group-append">
-                <span class="input-group-text" id="basic-addon2">id</span>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="0" aria-label="Variable"
-                aria-describedby="basic-addon2" id="variableValue"  >
-            <div class="input-group-append">
-                <span class="input-group-text" id="basic-addon2" >Val
-            </div>
-        </div>
-    </div>
-</div>`
-    card.appendChild(body)
-    return card
-}
-
-
-
-function forViewGenerator(parentID, statement) {
-    bodyId = 'body' + Date.now()
-    card = statementContainerView(parentID, statement, statement, bodyId)
-    //console.log(card.innerHTML)
-    var body = card.querySelector('#' + bodyId)
-    //console.log('body', body, bodyId)
-    selectUniqueId = Date.now()
-    body.innerHTML = `
-    <div class="row">
-        <div class="col">
-            <div class="row">
-                <div class="col">
-                <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="x" aria-label="Variable"
-                    aria-describedby="basic-addon2" id="variableId" value ="x">
-                <div class="input-group-append">
-                    <span class="input-group-text" id="basic-addon2">from</span>
-                </div>
-            </div>
-                </div>
-                <div class="col">
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="x" aria-label="Variable"
-                            aria-describedby="basic-addon2" id="forTo" value ="y.length">
-                        <div class="input-group-append">
-                            <span class="input-group-text" id="basic-addon2">to</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="0" aria-label="Variable"
-                            aria-describedby="basic-addon2" id="forIncrement"  value ="i++">
-                        <div class="input-group-append">
-                            <span class="input-group-text" id="basic-addon2" >increment</div>
-                    </div>
-                </div>
-            </div>
-                <div class="row mt-3">
-                    <h5>Nest Statement</h5>
-                    <div class="input-group mb-3">
-                        <select class="form-select" id="`+ selectUniqueId + `">
-                            <option value = "for" >for</option>
-                            <option value="switch" disabled>switch</option>
-                            <option value="if" disabled>if</option>
-                            <option value="method" disabled>method</option>
-                            <option value="variable" selected>variable</option>
-                        </select>
-                        <label class="input-group-text" for="`+ selectUniqueId + `"
-                            onclick="addStatementView(this.htmlFor,'`+ bodyId + `')">Add</label>
-                    </div>
-                </div>
-        </div>
-    </div>
-    `
-    card.appendChild(body)
-    return card
-}
+var mappedTree
 
 function addStatementView(selectId, parentID) {
     console.log("PARENT ID addStatementView", parentID)
@@ -150,7 +27,14 @@ function addStatementView(selectId, parentID) {
 
 function parseCodeSection(sectionId) {
     codeContainer = document.getElementById(sectionId)
-    tree = getTreeCode(codeContainer)
+    flatTree = getFlatArrTreeCode(codeContainer)
+    tree = list_to_tree(flatTree, 'codeSection')
+    console.log("TREE", flatTree)
+    mappedTree = flatTree.reduce(function(r, e) {
+        r[e.id] = e;
+        return r;
+        }, {});
+    console.log("MAPPED TREE :", mappedTree)
     tree = visitTree(tree, generateStatementCode)
     console.log("tree with code:", tree )
 }
@@ -159,10 +43,16 @@ function visitTree(tree,fn){
     for(i=0; i < tree.length; i++){
         if(tree[i].children.length>0){
             console.log(tree[i].children)
-            visitTree(tree[i].children,(f)=>{})
+            visitTree(tree[i].children,generateStatementCode)
         }
         else{
-            fn(tree, tree[i],'codeSection')
+            item =fn(tree, tree[i],'codeSection')
+            if(item.parentId != 'codeSection'){
+                mappedTree[item.parentId].bodyCode = mappedTree[item.parentId].bodyCode +'\n'+ item.fullCode
+                tree.pop(tree[i])
+                visitTree(tree,generateStatementCode)
+                console.log('mappedtree item: ',mappedTree[item.parentId])
+            } 
         }
     }
     return tree
@@ -170,7 +60,7 @@ function visitTree(tree,fn){
 
 
 
-function getTreeCode(codeContainer) {
+function getFlatArrTreeCode(codeContainer) {
     codeArr=[]
     let list = []
     codeArr = walkDom2(codeContainer, 'card-body');
@@ -180,10 +70,10 @@ function getTreeCode(codeContainer) {
         obj ["parentId"] = codeArr[i].getAttribute("parentCode")
         obj ["statement"] = codeArr[i].getAttribute("stype")
         obj ["bodyCode"] = ""
+        obj ["fullCode"] = ""
         list.push(obj)  
     }
-    tree = list_to_tree(list, 'codeSection')
-    return tree
+    return list
 }
 
 function list_to_tree(list,root) {
@@ -242,18 +132,17 @@ function generateStatementCode(tree, item, root) {
         case "variable":
             console.log('switch generate statement:' + statement)
             item.bodyCode = "variable code"
-            if(item.parentId =! root){
-
-            }
-            //getVariableDeclarationCode(id_element)
-            break;
+            item = getVariableDeclarationCode(item)
+            console.log(item)
+            return item
+            //addCodeToParent(tree,item)
         case "if":
             console.log('switch generate statement:' + statement)
             break;
         case "for":
             console.log('switch generate statement:' + statement)
-            item.bodyCode = "for body code"
-            break;
+            item.bodyCode = '{body}'
+            return item =getForCode(item)
         case "switch":
             console.log('switch generate statement:' + statement)
             break;
@@ -263,15 +152,34 @@ function generateStatementCode(tree, item, root) {
     }
 }
 
-function getVariableDeclarationCode(element) {
+function addCodeToParent(tree){
+    if(item.parentId!='codeSection'){
+        parent.bodyCode = parent.bodyCode + child.fullCode
+    }
+}
+
+function getVariableDeclarationCode(item) {
     //console.log('getVariableDeclarationCode(element) class:' + element.className)
+    element = document.getElementById(item.id)
     variableType = element.querySelector('#variableType')
     variableId = element.querySelector('#variableId')
     variableValue = element.querySelector('#variableValue')
     code = variableType.value + ' ' + variableId.value + ' ' + '=' + ' ' + variableValue.value + ';'
-    console.log('code' + code)
+    //console.log('code' + code)
+    item.fullCode = code
+    return item
 }
 
-function getForCode() {
-    console.log('not_implemented yet')
+function getForCode(item) {
+/*for(i=0; i< item.children.length; i++){
+    body = body + '\n' + item.children[i];
+}*/
+body = item.body
+code = `for (int i = 0; i < 100; i += 2)
+        {
+        `+body+`
+        }
+        `
+        item.fullCode = code
+        return item
 }
---
+++ resyduo_web_app/src/static/js/viewgen.js
@@ -0,0 +1,124 @@
+function statementContainerView(parentID, cardName, stype, bodyId) {
+    //var parent = document.getElementById(parentID)
+    var card = document.createElement('div')
+    card.className = "card bg-light mb-3"
+    card.id = stype
+    card.innerHTML = `
+    <div class="card-header justify-content-between">
+    <div class = "row">    
+        <div class = "col">
+                <h5>`+ cardName + `</h5>    
+            </div>
+            <div class = "col d-flex justify-content-end align-items-center">
+                <span class="badge text-bg-secondary" onClick = "this.parentNode.parentNode.parentNode.parentNode.remove()">&#x2715</span>
+            </div>
+        </div>
+    </div>
+    <div class="card-body" id="`+ bodyId + `" stype="` + stype + `" parentCode ="`+parentID+`" >
+    </div>
+    `
+    return card
+}
+
+
+function variableViewGenerator(parentID, statement) {
+    bodyId = 'body' + Date.now()
+    card = statementContainerView(parentID, statement, statement, bodyId)
+    var body = card.querySelector('#' + bodyId)
+    body.innerHTML = `
+<div class="row">
+    <div class="col">
+        <div class="input-group mb-3">
+            <select class="form-select" id="variableType">
+                <option selected value="int">int</option>
+                <option value="char">char</option>
+                <option value="long">long</option>
+                <option value="float">float</option>
+                <option value="variable">double</option>
+            </select>
+            <label class="input-group-text" for="inputGroupSelectLibrariesTab1">Type</label>
+        </div>
+    </div>
+    <div class="col">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="x" aria-label="Variable"
+                aria-describedby="basic-addon2" id="variableId" value ="x">
+            <div class="input-group-append">
+                <span class="input-group-text" id="basic-addon2">id</span>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="0" aria-label="Variable"
+                aria-describedby="basic-addon2" id="variableValue"  >
+            <div class="input-group-append">
+                <span class="input-group-text" id="basic-addon2" >Val
+            </div>
+        </div>
+    </div>
+</div>`
+    card.appendChild(body)
+    return card
+}
+
+
+
+function forViewGenerator(parentID, statement) {
+    bodyId = 'body' + Date.now()
+    card = statementContainerView(parentID, statement, statement, bodyId)
+    //console.log(card.innerHTML)
+    var body = card.querySelector('#' + bodyId)
+    //console.log('body', body, bodyId)
+    selectUniqueId = Date.now()
+    body.innerHTML = `
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="x" aria-label="Variable"
+                    aria-describedby="basic-addon2" id="variableId" value ="x">
+                <div class="input-group-append">
+                    <span class="input-group-text" id="basic-addon2">from</span>
+                </div>
+            </div>
+                </div>
+                <div class="col">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="x" aria-label="Variable"
+                            aria-describedby="basic-addon2" id="forTo" value ="y.length">
+                        <div class="input-group-append">
+                            <span class="input-group-text" id="basic-addon2">to</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="0" aria-label="Variable"
+                            aria-describedby="basic-addon2" id="forIncrement"  value ="i++">
+                        <div class="input-group-append">
+                            <span class="input-group-text" id="basic-addon2" >increment</div>
+                    </div>
+                </div>
+            </div>
+                <div class="row mt-3">
+                    <h5>Nest Statement</h5>
+                    <div class="input-group mb-3">
+                        <select class="form-select" id="`+ selectUniqueId + `">
+                            <option value = "for" >for</option>
+                            <option value="switch" disabled>switch</option>
+                            <option value="if" disabled>if</option>
+                            <option value="method" disabled>method</option>
+                            <option value="variable" selected>variable</option>
+                        </select>
+                        <label class="input-group-text" for="`+ selectUniqueId + `"
+                            onclick="addStatementView(this.htmlFor,'`+ bodyId + `')">Add</label>
+                    </div>
+                </div>
+        </div>
+    </div>
+    `
+    card.appendChild(body)
+    return card
+}
